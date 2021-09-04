59d5c460339c668829d3121a2682501a4d907697
michaelkotor <milan3020@gmail.com>
2021-09-04 03:51:24 +0300

    event (text editing)

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun doSomething(view: View) {
+        val textView: TextView = findViewById(R.id.textView);
+        val message: String = "Number: " + Math.random();
+        textView.text = message;
+        Toast.makeText(this, "Text changed", Toast.LENGTH_SHORT).show()
     }
 }
